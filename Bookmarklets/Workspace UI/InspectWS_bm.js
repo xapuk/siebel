634e097c0c4022faf7eea468a7c7593562796012
Viacheslav Babkin <vbabkin@gmail.com>
2020-10-07 00:25:28 +1100

    new placeholder and instruction changes

--- Bookmarklets/Workspace UI/InspectWS_bm.js
+++ Bookmarklets/Workspace UI/InspectWS_bm.js
@@ -1,3 +1,31 @@
+/* 
+@desc Inspect Workspace UI
+@author VB(xapuk.com)
+@version 1.1 2020/10/03
+@requires "FWK Runtime" business service to be published (Application ClientBusinessService usep property)
+@features
+    +elements: help text hidden by default, input field with the history, message bar, 3 buttons
+    +don't accept value shorter then 3 chars excluding *
+    +async call with busy overlay
+    +highlight search pattern in all found results
+    +shows a text if number of results = limit
+    +cut history to 5 items and don't store empty requests
+    +insect ws on click of <a>
+    +close on right-click of whitespace
+    +change a default ws filter to only filter out Delivered WSs
+    +copy ws name on right-click of link
+    +make a ws name in a sucess message a link
+    +put a timestamp in the message
+    +fix contextmenu on text input
+    +before opening a dialog check if it exists, and if so run auto inspect
+    +clicking a ws name inspects the first in the list
+    +dialog should have a unique selector itself so we don't mess with other dialogs
+    +print a message before server call, like inspecting ws blabla, or searching for workspace blabla
+    +use a function to print the message, keep a history of 3 messages
+    +close when click outside
+    +make it work and test in IE/Edge/Firefox
+    +ES6 => Babel => ES5 => Bookmarklet
+*/
 (() => {
     if ("undefined" === typeof SiebelApp) {
         alert("It works only in Siebel OUI session!");
@@ -19,8 +47,10 @@
         $("." + func).find(id + "IBtn").click();
         return;
     }
-    const help = `<i><p>Welcome to Inspect Workspace UI</p>Text field accepts different formats:<br><ul><li> - an exact workspace name: vbabkin_20200924_d419_1</li><li> - a search pattern of workspace name: *d419*</li><li> - an exact search spec for Repository Workspace BC: [Parent Name] = "Release 21" AND [Created By] = LoginId()</li><li> - leave it empty to search / inspect most recent undelivered workspaces created by active user</li></ul><p>Hit Enter to search for 10 most recent workspaces matching the provided name/pattern/spec and then click one of the workspaces in the list to inspect it. Hit Ctrl+Enter to inspect the most recent workspaces matching the provided name/pattern/spec.</p><p>If you just want to inspect/re-inspect your recent undelivered workspace, just hit Ctrl+Enter upon opening a dialog or double click a bookmark link.</p><p>Right click on workspace name will copy the name or right-click whitespace to close the dialog.</p><p>Check out <a href="http://xapuk.com/index.php?topic=125">http://xapuk.com/</a> for details.</p></i>`;
-    const html = `<div title="Inspect Workspace"><span id = "${func}Help" style = "display:none">${help}</span><input placeholder = "<my recent in-progress workspace>" type="text" id = "${func}" list="${func}History" autocomplete="off"><p id = "${func}Msg"></p><ul id="${func}List"></ul><datalist id = "${func}History"></datalist><style>.${func} input {width: 100%!Important;}#${func}List{margin-left: 15px;}#${func}Help i{font-size: 0.8rem;}.${func} li {list-style-type: disc;margin-left: 30px;}</style></div>`;
+
+    const help = `<i><p>Welcome to Inspect Workspace UI</p>The text field accepts different formats:<br><ul><li> - an exact workspace name: vbabkin_20200924_d419_1</li><li> - a search pattern of workspace name: *d419*</li><li> - an exact search spec for Repository Workspace BC: [Parent Name] = "Release 21" AND [Created By] = LoginId()</li><li> - leave it empty to search / inspect most recent undelivered workspaces created by the active user</li></ul><p>Hit Enter to search for 10 most recent workspaces matching the provided name/pattern/spec and then click one of the workspaces in the list to inspect it. Hit Ctrl+Enter to inspect the most recent workspaces matching the provided name/pattern/spec.</p><p>If you just want to inspect/re-inspect your recent undelivered workspace, just hit Ctrl+Enter upon opening a dialog or double click a bookmark link.</p><p>Right-click on workspace name will copy the name or right-click whitespace to close the dialog.</p><p>Check out <a href="http://xapuk.com/index.php?topic=125">http://xapuk.com/</a> for details.</p></i>`;
+    const html = `<div title="Inspect Workspace"><span id = "${func}Help" style = "display:none">${help}</span><input placeholder = "<my recent undelivered workspace>" type="text" id = "${func}" list="${func}History" autocomplete="off"><p id = "${func}Msg"></p><ul id="${func}List"></ul><datalist id = "${func}History"></datalist><style>.${func} input {width: 100%!Important;}#${func}List{margin-left: 15px;}#${func}Help i{font-size: 0.8rem;}.${func} li {list-style-type: disc;margin-left: 30px;}</style></div>`;
+
     const $d = $(html).dialog({
         modal: true,
         width: 640,
