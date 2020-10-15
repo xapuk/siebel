75d2439bc9394c2cfb9fc98de1c8a545655e6774
Viacheslav Babkin <vbabkin@gmail.com>
2020-10-15 20:43:14 +1100

    version 1.4

--- Bookmarklets/Workspace UI/InspectWS.js
+++ Bookmarklets/Workspace UI/InspectWS.js
@@ -38,6 +38,10 @@
     +when searching for exact ws name, shouldn't highlight it
     +link click doesn't work if clicked on highlighted part (<b>)
     +don't close on whitespace click
+@Fixed in 1.4
+    +change to the layout
+    +fixed empty call problem
+    +help text changes
 */
 (() => {
     if ("undefined" === typeof SiebelApp) {
@@ -72,14 +76,15 @@
     <p>Hit Enter to search for 10 most recent workspaces matching the provided name/pattern/spec and then click one of the workspaces in the list to inspect it.</p>
     <p>Hit Ctrl+Enter to inspect the most recent workspaces matching the provided name/pattern/spec.</p>
     <p>If you want to inspect/re-inspect your recent undelivered workspace, just hit Ctrl+Enter upon opening a dialog or double click a bookmark link.</p>
-    <p>Right click on workspace name will copy the name or right-click whitespace to close the dialog.</p>
+    <p>Right click on workspace name will copy the name.</p>
+    <p>Click anywhere outside of the dialog to close it.</p>
     <p>Check out <a href="http://xapuk.com/index.php?topic=125" target="_blank">http://xapuk.com/</a> for details.</p></i>`;
 
     const html = `<div title="Inspect Workspace">
             <span id = "${func}Help" style = "display:none">${help}</span>
             <input placeholder = "<${placeholder}>" type="text" id = "${func}" list="${func}History" autocomplete="off">
-            <p id = "${func}Msg"></p>
             <ul id="${func}List"></ul>
+            <p id = "${func}Msg"></p>
             <datalist id = "${func}History"></datalist>
             <style>
                 .${func} input {
